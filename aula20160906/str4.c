5acb256903aa7defeb2c6da1695e83ca97090970
Kerolayne Silva <kerolaynemenesesphb@hotmail.com>
2016-10-06 16:01:38 -0300

    Update str4.c

--- aula20160906/str4.c
+++ aula20160906/str4.c
@@ -33,6 +33,32 @@ int main(){
         frase[i] = 'y';
         if(frase[i] == 'm')
         frase[i] = 'z';
+        if(frase[i] == 'A')
+        frase[i] = 'N';
+        if(frase[i] == 'B')
+        frase[i] = 'O';
+        if(frase[i] == 'C')
+        frase[i] = 'P';
+        if(frase[i] == 'D')
+        frase[i] = 'Q';
+        if(frase[i] == 'E')
+        frase[i] = 'R';
+        if(frase[i] == 'F')
+        frase[i] = 'S';
+        if(frase[i] == 'G')
+        frase[i] = 'T';
+        if(frase[i] == 'J')
+        frase[i] = 'U';
+        if(frase[i] == 'I')
+        frase[i] = 'V';
+        if(frase[i] == 'J')
+        frase[i] = 'W';
+        if(frase[i] == 'K')
+        frase[i] = 'X';
+        if(frase[i] == 'L')
+        frase[i] = 'Y';
+        if(frase[i] == 'M')
+        frase[i] = 'Z';
     }
     printf("Frase codificada: %s\n", frase);
     return 0;

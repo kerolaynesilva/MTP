108774212e503688ebb546e48566cc49c1ca5b3e
Kerolayne Silva <kerolaynemenesesphb@hotmail.com>
2016-10-08 15:13:13 -0300

    Update stk2.c

--- aula20161004/stk2.c
+++ aula20161004/stk2.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
 #include <math.h>
-
-float media(int num1,int num2,int num3,int num4,int num5);
+float media(float num1,float num2,float num3,float num4,float num5);
+void desvio_padrao(float media,float num1,float num2,float num3,float num4,float num5);
 int main(){
-    int num1,num2,num3,num4,num5;
+    float num1,num2,num3,num4,num5;
+    float x;
     printf("Digite 5 numeros: ");
-    scanf("%d", num1);
-    scanf("%d", num2);
-    scanf("%d", num3);
-    scanf("%d", num4);
-    scanf("%d", num5);
-    printf("Media = ", media(num1,num2,num3,num4,num5));
+    scanf("%f", &num1);
+    scanf("%f", &num2);
+    scanf("%f", &num3);
+    scanf("%f", &num4);
+    scanf("%f", &num5);
+    x = media(num1,num2,num3,num4,num5);
+    printf("Media = %.1f\n", x);
+    desvio_padrao(x,num1,num2,num3,num4,num5);
     return 0;
 }
-float media(int num1,int num2,int num3,int num4,int num5){
-    int i;
+float media(float num1,float num2,float num3,float num4,float num5){
     float soma,media;
     soma = num1+num2+num3+num4+num5;
     media = soma/5;
     return media;
 }
-void desvio_padrao(float media,int num1,int num2,int num3,int num4,int num5){
+void desvio_padrao(float media,float num1,float num2,float num3,float num4,float num5){
     float desvio;
-    desvio = sqrt((pow(media-num1),2)+pow(media-num2),2)+pow(media-num3),2)+pow(media-num4),2)+pow(media-num5),2))/4);
-
+    desvio = sqrt((pow((media-num1),2)+pow((media-num2),2)+pow((media-num3),2)+pow((media-num4),2)+pow((media-num5),2))/4);
+    printf("Desvio padrao = %.4f\n", desvio);
 }
+
+
